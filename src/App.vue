fc57f9751cc158d1c8136062207fe88d002e36d2
Matheus Carvalho <matheus.santos.hcs@gmail.com>
2021-01-05 15:29:50 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script>
+import Layout from './components/Layout.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Layout
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <Layout />
+  </div>
+</template>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body {
+    background: var(--background);
+    color: var(--text);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Roboto', serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  :root {
+    --primary: #5e72e4;
+    --background: #f8f8fb;
+    --text: #141414;
+    --white: #fff;
+    --gray: #9c9c9c;
+    --green: #266d4d;
+  }
+</style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,18 @@
+<script>
+
+export default {
+  name: 'Layout',
+}
+</script>
+
+<template>
+  <div class="container">
+    Hello World
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    font-size: 20px;
+  }
+</style>
